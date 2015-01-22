a7f697859a615f960b6e660c93ac59589ce6d8da
Jodie Belgrave <rmb1993@gmail.com>
2015-01-21 19:15:44 -0500

    copy before initial run

--- Puppet/installPuppetMaster.sh
+++ Puppet/installPuppetMaster.sh
@@ -2,7 +2,7 @@
 
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yum install puppet-server
+cp master/puppet.conf /etc/puppet/puppet.conf
 puppet master --verbose --no-daemonize
 chkconfig puppetmaster on
 service puppetmaster start
-cp master/puppet.conf /etc/puppet/puppet.conf
